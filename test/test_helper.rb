1f5c6065fc3f5536243c42b5edc16caf99ea34ca
tadman <github@tadman.ca>
2009-04-22 17:04:40 -0400

    Initial commit to mysql-replication-helper.

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'test/unit'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'mysql_replication_helper'
+
+class Test::Unit::TestCase
+end
